911a3bc73733955fff491f96ad671d64a05c36d9
bobbysingla1112@gmail.com <aggarwal12345*>
2021-07-28 22:08:37 +0530

    first commit

---
+++ src/app/Mycomponents/todos/todos.component.ts
@@ -0,0 +1,51 @@
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+ 
+  localItem:string;
+  todos: Todo[];
+  constructor() {
+    this.localItem=localStorage.getItem("todos");
+    if(this.localItem==null)
+    this.todos =[];
+    else
+    this.todos=JSON.parse(this.localItem)
+  }
+  ngOnInit(): void {
+  }
+
+  deleteTodo(todo:Todo){
+
+    const index=this.todos.indexOf(todo);
+    this.todos.splice(index,1)
+    localStorage.setItem("todos",JSON.stringify(this.todos));
+    console.log(todo)
+  }
+
+ 
+  
+   addTodo(todo:Todo){
+    this.todos.push(todo)
+    
+    localStorage.setItem("todos",JSON.stringify(this.todos));
+    console.log(todo)
+  }
+
+
+  toggleTodo(todo:Todo){
+
+    const index=this.todos.indexOf(todo);
+    this.todos[index].active=!this.todos[index].active;
+     localStorage.setItem("todos",JSON.stringify(this.todos));
+    console.log(todo)
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './Mycomponents/todos/todos.component';
+import { TodoItemComponent } from './Mycomponents/todo-item/todo-item.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './Mycomponents/about/about.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddTodoComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
